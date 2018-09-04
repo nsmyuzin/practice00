404d6e07e09d14ae8405e8a52571aa812458aeb9
snishizawa0509 <s-nishizawa@towatech.net>
2018-09-04 15:58:32 +0900

    setting to use ckeditor on active_admin
    まだ設定不足があるかもしれないので要確認
    README

--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -1,2 +1,4 @@
 #= require active_admin/base
 #= require active_admin/sortable
+##本番環境でも動くように
+CKEDITOR_BASEPATH = '/assets/ckeditor/';
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -199,7 +199,8 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_print_stylesheet.css', media: :print
   #
   # To load a javascript file:
-  #   config.register_javascript 'my_javascript.js'
+  # JSファイルをロードする
+  config.register_javascript 'ckeditor/init.js'
 
   # == CSV options
   #
