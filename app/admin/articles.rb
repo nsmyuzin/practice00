f28e5a3dd820dbfb63dad9b1fe39ba9a24d05aa5
snishizawa0509 <s-nishizawa@towatech.net>
2018-09-04 12:38:52 +0900

    CAN selectboxでカテゴリ選び

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,16 +1,27 @@
 ActiveAdmin.register Article do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-permit_params :title, :sentence
-#
-# or
-#
+  permit_params :title, :sentence, :category_id
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :category_id,
+        as: :select,
+        collection: Category.tree_type_order.map { |c| [c.name, c.id] }
+        #collection: Category.tree_type_order.map { |c| ['-' * c.depth + c.name, c.id] }
+      f.input :sentence
+    end
+    f.semantic_errors
+    f.actions
+  end
+
+  controller do
+  end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,23 @@ class Category < ApplicationRecord
   has_closure_tree ##閉包テーブルclosure_tree用
   has_many :articles
   validates :name, presence: true, uniqueness: true
+
+  ##tree表示用
+  def self.tree_type_order
+    ##postgres用
+    query = <<-SQL
+  SELECT c.id,CONCAT(depth,c.name) AS name
+  FROM (
+  SELECT h.descendant_id,
+  STRING_AGG('','-') AS depth,
+  STRING_AGG(LPAD(CAST(h.ancestor_id AS text), 2, '0'), '-' ORDER BY h.generations DESC) AS path
+  FROM category_hierarchies AS h
+  GROUP BY h.descendant_id
+  ) AS t
+  INNER JOIN categories AS c
+  ON c.id = t.descendant_id
+  ORDER BY path asc
+    SQL
+    self.find_by_sql(query)
+  end
 end
