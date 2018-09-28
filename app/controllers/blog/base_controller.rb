a041448f0193e60223a50637af9cdd18bbae1647
snishizawa0509 <s-nishizawa@towatech.net>
2018-09-28 10:14:33 +0900

    create banner template

--- app/controllers/blog/base_controller.rb
+++ app/controllers/blog/base_controller.rb
@@ -1,10 +1,15 @@
 class Blog::BaseController < ApplicationController
   before_action :load_default
+  before_action :load_banner #TODO fix
 
   private
   def load_default
     @new_articles = Article.limit(5)
     @categories = Category.all
+  end
+  def load_banner
+    @right_banner= Banner.find(1)
+    @left_banner= Banner.find(2)
   end
 
 end
